b4e818af8db1f22d38804c9b7374a7e74e8ae4f7
Robert <r.zioltkowski@gmail.com>
2024-07-10 18:24:33 +0200

    rdy4launch, maybe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,19 +29,19 @@
 	<!-- About -->
 	<div
 		id="ueberuns"
-		class="flex flex-col items-center justify-between lg:min-h-screen lg:flex-row-reverse"
+		class="grid min-h-screen items-end lg:grid lg:scroll-mt-0 lg:grid-cols-2 lg:items-center"
 	>
-		<div class="p-5">
+		<div class="p-5 lg:order-2">
 			<h3 class="mb-5 text-2xl font-bold">Über Uns</h3>
 			<div class="prose">{@html data.was_machen_wir[0].text}</div>
 		</div>
-		<img src="/images/am_wasser.jpg" alt="am wasser" class="lg:w-[90%] lg:rounded lg:drop-shadow" />
+		<img src="/images/am_wasser.jpg" alt="am wasser" class="lg:order-1 lg:rounded lg:drop-shadow" />
 	</div>
 
 	<!-- Leistungen -->
 	<div
 		id="leistungen"
-		class="flex flex-col items-center justify-between lg:min-h-screen lg:flex-row"
+		class="grid min-h-screen items-end lg:grid lg:scroll-mt-0 lg:grid-cols-2 lg:items-center"
 	>
 		<div class="p-5">
 			<h3 class="mb-5 text-2xl font-bold">Unsere Leistungen</h3>
@@ -51,15 +51,15 @@
 				{/each}
 			</ul>
 		</div>
-		<img src="/images/proben.jpg" alt="proben" class="lg:w-[90%] lg:rounded lg:drop-shadow" />
+		<img src="/images/proben.jpg" alt="proben" class="lg:rounded lg:drop-shadow" />
 	</div>
 
 	<!-- Projekte -->
 	<div
 		id="projekte"
-		class="flex flex-col items-center justify-between lg:min-h-screen lg:flex-row-reverse"
+		class="grid min-h-screen items-end lg:grid lg:scroll-mt-0 lg:grid-cols-2 lg:items-center"
 	>
-		<div class="p-5">
+		<div class="p-5 lg:order-2">
 			<h3 class="mb-5 text-2xl font-bold">Wo sind wir im Einsatz?</h3>
 			<div class="prose">
 				{@html data.wo_sind_wir_im_einsatz[0].text}
@@ -68,12 +68,15 @@
 		<img
 			src="/images/umspannwerk.jpg"
 			alt="umspannwerk"
-			class="lg:w-[90%] lg:rounded lg:drop-shadow"
+			class="lg:order-1 lg:rounded lg:drop-shadow"
 		/>
 	</div>
 
 	<!-- Kontakt -->
-	<div id="kontakt" class="flex flex-col items-center justify-between lg:min-h-screen lg:flex-row">
+	<div
+		id="kontakt"
+		class="grid min-h-screen items-end lg:grid lg:scroll-mt-0 lg:grid-cols-2 lg:items-center"
+	>
 		<div class="flex flex-col gap-5 p-5">
 			<h3 class="mb-5 text-2xl font-bold">Kontakt</h3>
 			<div class="prose">
@@ -97,17 +100,17 @@
 		<img
 			src="/images/chef2.jpg"
 			alt="chef"
-			class="h-[400px] w-[400px] lg:rounded-full lg:drop-shadow"
+			class="lg:h-[400px] lg:w-[400px] lg:rounded-full lg:drop-shadow"
 		/>
 	</div>
 
 	<!-- Zitat -->
-	<div class="mb-96 mt-20">
+	<div class="mb-60 mt-40 px-5">
 		<div class="text-center text-lg italic lg:text-xl">
 			"Zuverlässige Arbeit, präzise Messungen und zügige Ergebnisse. Das macht uns aus."
 		</div>
-		<div class="mt-3 text-center lg:mt-5 lg:text-lg">
-			- Ihr Ansprechpartner, Dipl.-Ing. Adam Zioltkowski
+		<div class="mt-5 text-center lg:text-lg">
+			- Ihr Ansprechpartner, <br /> Dipl.-Ing. Adam Zioltkowski
 		</div>
 	</div>
 </div>
