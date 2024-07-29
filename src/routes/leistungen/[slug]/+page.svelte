2e5ebb6141356e27161c5aa36f7b9b8f0084ad30
Robert <r.zioltkowski@gmail.com>
2024-07-29 18:05:11 +0200

    feat: download documents in leistungen

--- src/routes/leistungen/[slug]/+page.svelte
+++ src/routes/leistungen/[slug]/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { env } from '$env/dynamic/public'
+	import { PUBLIC_BACKEND_URL } from '$env/static/public'
 	import type { PageData } from './$types'
 	export let data: PageData
 </script>
@@ -11,7 +13,21 @@
 	/>
 </svelte:head>
 
-<div class="flex flex-col items-center p-5">
+<div class="mb-5 flex flex-col items-center p-5">
 	<div class="my-5 text-2xl font-semibold">{data.title}</div>
 	<div class="prose">{@html data.text}</div>
+	<div class="">
+		<div>
+			{#if data.download}
+				<div class="text-xl font-semibold">Dokumente zum Herunterladen:</div>
+				{#each data.download as download}
+					<a
+						href={`${PUBLIC_BACKEND_URL}/api/files/${data.collectionId}/${data.id}/${download}`}
+						download
+						class="prose my-2 block rounded-md bg-gray-200 p-2">{download}</a
+					>
+				{/each}
+			{/if}
+		</div>
+	</div>
 </div>
